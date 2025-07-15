6365f3a526dab523b6d25ca50e43ac88bda899e7
Vishal200311 <gojegavevishal@gmail.com>
2025-07-15 10:55:18 +0530

    second commit

---
+++ sum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,add;
+    printf("enter the two number");
+    scanf("%d%d",&a,&b);
+    add = a+b;
+    printf("add the number: %d",add);
+    return 0;
+}
