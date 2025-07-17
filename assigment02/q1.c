06cff2c95929a47ae9bdf07175845ee022ff844e
Vishal200311 <gojegavevishal@gmail.com>
2025-07-17 22:54:09 +0530

    number

---
+++ assigment02/q1.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+int main()
+{
+    int num;
+    printf("enter the number :");
+    scanf("%d",&num);
+    if(num>=0){
+        printf(" number is positive");
+    }else if("num<=0"){
+        printf(" number is negative");
+    }else 
+    printf("enter the number:%.2d",num);
+    return 0;
+}
