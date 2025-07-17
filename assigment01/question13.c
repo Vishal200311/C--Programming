b42ad80612b0ad387ee37c1cdb099228e0a1d7b3
Vishal200311 <gojegavevishal@gmail.com>
2025-07-17 20:06:25 +0530

    ascii

---
+++ assigment01/question13.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+    printf("enter a character:");
+    scanf("%c",&ch);
+    printf("enter the ascii value of character %c : %d\n",ch,ch);
+    return 0;
+
+}
